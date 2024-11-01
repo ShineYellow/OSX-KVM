7e9f48993f35430993dbf761fed9c61018d33f88
Angelo Cassano <angy.avv.94@hotmail.it>
2024-11-01 18:05:47 +0100

    Added support to macOS 15 Sequoia

--- fetch-macOS-v2.py
+++ fetch-macOS-v2.py
@@ -530,7 +530,8 @@ def main():
             {"name": "Big Sur (11.7) - RECOMMENDED", "b": "Mac-2BD1B31983FE1663", "m": "00000000000000000", "short": "big-sur"},
             {"name": "Monterey (12.6)", "b": "Mac-B809C3757DA9BB8D", "m": "00000000000000000", "os_type": "latest", "short": "monterey"},
             {"name": "Ventura (13)", "b": "Mac-4B682C642B45593E", "m": "00000000000000000", "os_type": "latest", "short": "ventura"},
-            {"name": "Sonoma (14) ", "b": "Mac-A61BADE1FDAD7B05", "m": "00000000000000000", "short": "sonoma"}
+            {"name": "Sonoma (14) ", "b": "Mac-A61BADE1FDAD7B05", "m": "00000000000000000", "short": "sonoma"},
+            {"name": "Sequoia (15) ", "b": "Mac-7BA5B2D9E42DDD94", "m": "00000000000000000", "short": "sonoma"}
     ]
     for index, product in enumerate(products):
         name = product["name"]
